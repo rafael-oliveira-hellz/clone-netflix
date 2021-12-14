11aaafb048f20a59edaaefbc455b61786fc9fb98
Rafael Oliveira <rdeoliveirasilva3@gmail.com>
2021-12-14 18:19:32 -0300

    + Connected to TMDB API and fetched the movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
+export default () => {
 
-export default App;
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista completa de filmes
+
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pegando o Filmes em Destaque
+
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomMovie = Math.floor(Math.random() * (originals[0].items.results.length));
+      let chosenMovie = originals[0].items.results[randomMovie];
+      let chosenMovieInfo = await Tmdb.getMovieInfo(chosenMovie.id, 'tv');
+
+      setFeaturedData(chosenMovieInfo);
+    }
+
+    loadAll();
+
+  }, []);
+
+  return (
+    <div className="page">
+
+      {featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <div key={key}>
+            <MovieRow key={key} title={item.title} items={item.items} />
+          </div>
+        ))}
+      </section>
+    </div>
+  )
+}
